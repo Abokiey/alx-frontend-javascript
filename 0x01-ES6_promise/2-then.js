30106eeaa4253e7ff2465586cc559ab3ff68c112
Abokiey <sweneysam@gmail.com>
2024-03-05 19:29:52 +0300

    2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,8 @@
-export default function handleResponseFromAPI(promise) {
+export default function handleResponseFromAPI (promise) {
   return promise.then(() => (
-    { status: 200, body: 'Success' }
-      .catch(() => new Error())
-      .finally(() => {
-        console.log('Got a response from the API');
-      })
-  ));
+    { status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
