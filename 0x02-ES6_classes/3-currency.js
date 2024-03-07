0b4d8bcc21508e2549c4d663ff4e74be529e56a5
Abokiey <sweneysam@gmail.com>
2024-03-07 17:59:02 +0300

    3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,30 +1,26 @@
 export default class Currency {
   constructor(code, name) {
-    if (typeof name !== 'string') throw TypeError('must be a string');
-    if (typeof code !== 'string') throw TypeError('must be a string');
-    this._name = name;
     this._code = code;
-  }
-
-  get name() {
-    return `${this._name}`;
-  }
-
-  set name(name) {
-    if (typeof name !== 'string') throw TypeError('must be a string');
     this._name = name;
-  }
-
-  get code() {
-    return `${this._name}`;
   }
 
   set code(code) {
-    if (typeof code !== 'string') throw TypeError('must be a string');
     this._code = code;
   }
 
+  get code() {
+    return this._code;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
   displayFullCurrency() {
-    return `${this._name} (${this._code})`;
+    return `${this.name} (${this.code})`;
   }
 }
