f1befd61f0581cfae1e2484dfbfb41b78742d8cd
Abokiey <sweneysam@gmail.com>
2024-03-07 18:05:53 +0300

    2

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,14 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return `${this._amount}`;
+  }
+
+  set amount(amount)
+}
