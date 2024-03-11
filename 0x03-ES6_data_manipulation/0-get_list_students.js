603c792074a692f24a6a12d7cd268e4ce7742da4
Abokiey <sweneysam@gmail.com>
2024-03-11 15:43:54 +0300

    0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'san fransisco' },
+    { id: 2, firstName: 'James', location: 'columbia' },
+    { id: 3, firstName: 'Serena', location: 'san fransisco' },
+  ];
+}
