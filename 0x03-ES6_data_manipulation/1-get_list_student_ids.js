8295c51e38e681c04616935b79e1a71a3f66438a
Abokiey <sweneysam@gmail.com>
2024-03-12 12:14:31 +0300

    1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,7 @@
 // eslint-disable-next-line consistent-return
-export default Function getListStudentIds(array) {
-  if (!Array.isArray(array)) return [];
+export default function getListStudentIds(array) {
+  if (!Array.isArray(array)) {
+    return [];
+  }
   return array.map((student) => student.id);
 }
