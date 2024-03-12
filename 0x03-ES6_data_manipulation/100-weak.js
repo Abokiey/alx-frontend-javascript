3e3874ade12f759916eaa6d7e5d11a4f1e61a895
Abokiey <sweneysam@gmail.com>
2024-03-12 18:52:55 +0300

    100

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,7 +1,7 @@
 export const weakMap = new WeakMap();
 
 export const queryAPI = (endpoint) => {
-  const count = weakMap.get(endpoint) || 0;
+  let count = weakMap.get(endpoint) || 0;
   weakMap.set(endpoint, count + 1);
 
   if (count >= 4) throw console.error('Endpoint load is high');
